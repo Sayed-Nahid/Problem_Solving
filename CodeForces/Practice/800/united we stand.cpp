c0736de3481e9522208966678eae32208b21c4c1
Sayed Nahid <nahid15-3849@diu.edu.bd>
2024-03-27 07:18:15 +0600

    adding

---
+++ CodeForces/Practice/800/united we stand.cpp
@@ -0,0 +1,67 @@
+/*
+            Name: Sayed Nahid
+            CodeForces: Y0urFriend
+            email: nahid15-3849@diu.edu.bd
+            Daffodil International University
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef     long long int     ll;
+typedef     long double       ld;
+typedef     vector<ll>        vll;
+typedef     vector<int>       vint;
+#define     vsort(v)          sort(v.begin(), v.end())
+#define     vsortre(v)        sort(v.begin(), v.end(), greater<int>())
+#define     arrsort(arr, n)   sort(arr, arr+n)
+#define     arrsortre(arr, n) sort(arr, arr+n, greater<int>())
+#define     fi                first
+#define     print(v)          for(ll i:v) cout<<i<<ss
+#define     se                second
+#define     pb                push_back
+#define     e                 "\n"
+#define     all(p)            p.begin(),p.end()
+#define     zz(v)             (ll)v.size()
+#define     S(a)              scanf("%lld",&a)
+#define     SS(a,b)           scanf("%lld %lld",&a,&b)
+#define     SSS(a,b,c)        scanf("%lld %lld %lld",&a,&b,&c)
+#define     ss                ' '
+#define     pii               pair<ll,ll>
+#define     gcd(a,b)          __gcd(a,b)
+#define     lcm(a,b)          (a*b)/gcd(a,b)
+#define     setp(a)           fixed << setprecision(a)
+const double eps = 1e-7;
+
+void solve(){
+    bool f=false;
+    string s="";
+    ll x, y, i, j, k, n, m, a, b, c=0, len;
+    cin >> n;
+    vector<ll> v(n);
+    for(i=0; i<n; i++) cin >> v[i];
+    sort(v.begin(), v.end());
+    if(v[n-1]==v[0]) cout << -1 << e;
+    else{
+        ll it=0;
+        while(v[0]==v[it]) it++;
+        cout<< it << " " << n-it <<e;
+        for(i=0; i<it; i++) cout << v[i] << " \n"[i==it-1];
+        for(i=it; i<n; i++) cout << v[i] << " \n"[i==n-1];
+    }
+}
+
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
